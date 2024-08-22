662b8a34457f98c34da79f04e11d34d7eef993d4
Aliaksei Karzhou <alexius90e@gmail.com>
2024-08-22 11:24:52 +0300

    feat: added main menu

--- js/index.js
+++ js/index.js
@@ -1,34 +1,28 @@
 const headerBurgerButton = document.querySelector('.header__burger-button');
-const headerMenu = document.querySelector('.header__menu');
-const header = document.querySelector('.header');
+const mainMenu = document.querySelector('.main-menu');
 
 function showMenu() {
-  if (headerBurgerButton && headerMenu && header) {
-    header.classList.add('active');
-    headerMenu.classList.add('active');
+  if (headerBurgerButton && mainMenu) {
+    mainMenu.classList.add('active');
     document.body.classList.add('hidden_md');
   }
 }
 
 function hideMenu() {
-  if (headerBurgerButton && headerMenu && header) {
-    header.classList.remove('active');
-    headerMenu.classList.remove('active');
+  if (headerBurgerButton && mainMenu) {
+    mainMenu.classList.remove('active');
     document.body.classList.remove('hidden_md');
   }
 }
 
-if (headerBurgerButton && headerMenu && header) {
+if (headerBurgerButton && mainMenu) {
   headerBurgerButton.addEventListener('click', () => showMenu());
 
-  headerMenu.addEventListener('click', (event) => {
-    const isNavLink = event.target.classList.contains('header__menu-nav-link');
-    const isClose = event.target.classList.contains('header__menu-close');
-    if (isNavLink || isClose) hideMenu();
-  });
-
-  header.addEventListener('click', (event) => {
-    const isHeader = event.target === event.currentTarget;
-    if (isHeader) hideMenu();
+  mainMenu.addEventListener('click', (event) => {
+    const isMenu = event.currentTarget === event.target;
+    const isNavLink = event.target.classList.contains('main-menu__nav-link');
+    const isClose = event.target.classList.contains('main-menu__close');
+    const isLogin = event.target.classList.contains('main-menu__login-button');
+    if (isMenu || isNavLink || isClose || isLogin) hideMenu();
   });
 }
