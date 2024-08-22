81be2e40a4df3e0498baff447fd99d0dcbd77ef9
Aliaksei Karzhou <alexius90e@gmail.com>
2024-08-22 12:52:50 +0300

    feat: added contact modal

--- js/index.js
+++ js/index.js
@@ -26,3 +26,58 @@ if (headerBurgerButton && mainMenu) {
     if (isMenu || isNavLink || isClose || isLogin) hideMenu();
   });
 }
+
+const maskOptions = {
+  mask: '+{7} (000) 000-00-00',
+};
+
+const phoneInputClassNames = ['modal__form-input_tel'];
+
+const phoneInputs = phoneInputClassNames
+  .map((name) => document.querySelectorAll(`.${name}`))
+  .reduce((acc, array) => [...acc, ...array], []);
+
+phoneInputs.forEach((inputElement) => IMask(inputElement, maskOptions));
+
+const contactButtons = document.querySelectorAll('.contact');
+const contactModal = document.querySelector('.modal_contact');
+const contactForm = document.querySelector('.modal_contact .modal__form');
+
+function showModal(modal) {
+  if (modal === null) return;
+  modal.classList.add('active');
+  document.body.classList.remove('hidden_md');
+  document.body.classList.add('hidden');
+}
+
+function hideModal(modal) {
+  if (modal === null) return;
+  modal.classList.remove('active');
+  document.body.classList.remove('hidden_md');
+  document.body.classList.remove('hidden');
+}
+
+contactButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    showModal(contactModal);
+  });
+});
+
+if (contactModal) {
+  contactModal.addEventListener('click', (event) => {
+    const isModal = event.target === event.currentTarget;
+    const isClose = event.target.classList.contains('modal__close');
+
+    if (isModal || isClose) {
+      hideModal(event.currentTarget);
+    }
+  });
+}
+
+if (contactForm) {
+  contactForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    hideModal(contactModal);
+    event.currentTarget.reset();
+  });
+}
